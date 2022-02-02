769e1acf651d7724dfdde0cff817ec80e34a42f6
pandeysudip <sudip@siu.edu>
2022-02-01 22:30:55 -0800

    updating app.py

---
+++ app.py
@@ -0,0 +1,106 @@
+from flask import Flask, request, render_template, redirect, jsonify
+import json
+import os
+from model_param import model_load_cancer, model_load_asthma
+from bson import json_util
+from pymongo import MongoClient
+from flask_pymongo import PyMongo
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+client = MongoClient("mongodb://localhost:27017")
+db = client['air-quality']
+
+# os.environ.get('MONGO_URI', '')
+# app.config["MONGO_URI"] =
+#app.config['MONG_DBNAME'] = 'e-commerce'
+#mongo = PyMongo(app)
+
+# creating collection (add mongo)
+cancer = db['cancer']
+asthma = db['asthma']
+
+
+@app.route('/')
+def home():
+    # Return the template
+    return render_template('index.html')
+
+
+@app.route('/index.html')
+def index():
+    # Return the template
+    return render_template('index.html')
+
+
+@app.route('/dashboard.html')
+def figure():
+    # Store the entire collection as a list
+
+    # Return the template
+    return render_template('dashboard.html')
+
+
+@app.route('/contact.html')
+def contact():
+    # Return the template
+    return render_template('contact.html')
+
+
+@app.route('/data.html')
+def data():
+    # Return the template
+    return render_template('data.html')
+
+
+@app.route('/send/asthma', methods=["GET", "POST"])
+def predic_asthma():
+    if request.method == "POST":
+
+        Age_Group = request.form.get('Age_Group')
+        air_quality = request.form.get('air_quality')
+        variables = [Age_Group,
+                     air_quality]
+        predict = model_load_asthma(variables)
+
+        return render_template("index.html", pred=variables, prediction=predict)
+    else:
+        return render_template("index.html")
+
+
+@app.route('/send/cancer', methods=["GET", "POST"])
+def predic_cancer():
+    if request.method == "POST":
+
+        Coughing = request.form.get('Coughing')
+        Obesity = request.form.get("Obesity")
+        Smoker = request.form.get('Smoker')
+        Fatigue = request.form.get('Fatigue')
+        Alchol_use = request.form.get('Alchol_use')
+        Balanced_diet = request.form.get('ProductRelated_Duration')
+        Air_pollution = request.form.get('Air_pollution')
+        variables = [Coughing, Obesity,
+                     Smoker, Fatigue, Alchol_use,
+                     Balanced_diet, Air_pollution]
+        predict = model_load_cancer(variables)
+
+        return render_template("index.html", pred=variables, prediction=predict)
+    else:
+        return render_template("index.html")
+
+
+@ app.route("/data/cancer")
+def get_data():
+    all_data_list = list(cancer.find())
+    return json.dumps(all_data_list, default=json_util.default)
+
+
+@ app.route("/data/asthma")
+def get_data1():
+    all_data_list = list(asthma.find())
+    return json.dumps(all_data_list, default=json_util.default)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model_param.py
@@ -0,0 +1,60 @@
+import joblib
+import numpy as np
+
+# defining a function for combining two columns
+
+
+def avgDuration(c, d):
+    c = float(c)
+    d = float(d)
+    if d == 0 or c == 0:
+        result = 0
+    elif (c != 0) or (d != 0):
+        result = float(d)/float(c)
+    return result
+
+# defing model to predict based on our saved model
+
+
+def model_load_cancer(variables):
+    Administrative_Avg = avgDuration(variables[0], variables[1])
+    Informational_Avg = avgDuration(variables[2], variables[3])
+    ProductRelated_Avg = avgDuration(variables[4], variables[5])
+    BounceRates = variables[6]
+    ExitRates = variables[7]
+    PageValues = variables[8]
+    final_variables = [Administrative_Avg, Informational_Avg,
+                       ProductRelated_Avg, BounceRates, ExitRates, PageValues]
+    # using standardscaler for transformation
+    scaler = joblib.load('scaler.pkl')
+    X_scaler = scaler.transform([final_variables])
+
+    test_data = scaler.transform(X_scaler)
+    trained_model = joblib.load('model.pkl')
+    prediction = trained_model.predict(test_data)
+    if prediction == [0]:
+        return "Based on our trained model it looks like 'Customer will not BUY'"
+    else:
+        return "Based on our trained model it looks like 'Customer will BUY'"
+
+
+def model_load_asthma(variables):
+    Administrative_Avg = avgDuration(variables[0], variables[1])
+    Informational_Avg = avgDuration(variables[2], variables[3])
+    ProductRelated_Avg = avgDuration(variables[4], variables[5])
+    BounceRates = variables[6]
+    ExitRates = variables[7]
+    PageValues = variables[8]
+    final_variables = [Administrative_Avg, Informational_Avg,
+                       ProductRelated_Avg, BounceRates, ExitRates, PageValues]
+    # using standardscaler for transformation
+    scaler = joblib.load('scaler.pkl')
+    X_scaler = scaler.transform([final_variables])
+
+    test_data = scaler.transform(X_scaler)
+    trained_model = joblib.load('model.pkl')
+    prediction = trained_model.predict(test_data)
+    if prediction == [0]:
+        return "Based on our trained model it looks like 'Customer will not BUY'"
+    else:
+        return "Based on our trained model it looks like 'Customer will BUY'"
