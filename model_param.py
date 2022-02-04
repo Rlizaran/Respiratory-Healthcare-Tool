df36c149b2c187317da85d6b0b0a72b3d2176ab5
pandeysudip <sudip@siu.edu>
2022-02-04 15:15:18 -0800

    updating error of website

--- model_param.py
+++ model_param.py
@@ -6,25 +6,34 @@ import numpy as np
 
 
 def model_load_cancer(variables):
-    final_variables = [variables]
+
+    if variables[-1] == 'hidden':
+        return "Please Enter the Zip Code of Patients First!!!!"
     # loading model
-    trained_model = joblib.load('model_cancer.pkl')
-    prediction = trained_model.predict(final_variables)
-    if prediction == [0]:
-        return "There is very low chance of having cancer"
-    elif prediction == [1]:
-        return "There is medium chance of having cancer"
+    elif (variables[0] != int) or (variables[0] != int) or (variables[0] != int):
+        return "Please Enter the Above Information First!!!!"
     else:
-        return "There is very high chance of having cancer"
+        final_variables = [variables]
+        trained_model = joblib.load('model_cancer.pkl')
+        prediction = trained_model.predict(final_variables)
+        if prediction == [0]:
+            return "There is very low chance of having cancer"
+        elif prediction == [1]:
+            return "There is medium chance of having cancer"
+        else:
+            return "There is very high chance of having cancer"
 
 
 def model_load_asthma(variables):
     # using standardscaler for transformation
-    scaler = joblib.load('scaler_asthma.pkl')
-    X_scaler = scaler.transform([variables])
+    if variables[1] == 'hidden':
+        return "Please Enter the Zip Code of Patients First!!!"
+    else:
+        scaler = joblib.load('scaler_asthma.pkl')
+        X_scaler = scaler.transform([variables])
 
-    test_data = scaler.transform(X_scaler)
-    trained_model = joblib.load('model_asthma.pkl')
-    prediction = trained_model.predict(test_data)
-    prediction = round(prediction[0], 2)
-    return f'Emergency department visit rate: {prediction}'
+        test_data = scaler.transform(X_scaler)
+        trained_model = joblib.load('model_asthma.pkl')
+        prediction = trained_model.predict(test_data)
+        prediction = round(prediction[0], 2)
+        return f'Emergency department visit rate: {prediction}'
